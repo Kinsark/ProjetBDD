341f995f8f533658298f14f0d5af4edd004d5f7f
Cassius Garat <cassius.garat@phelma.grenoble-inp.fr>
2017-04-03 15:26:12 +0200

    Ajout de la fenetre des stats

---
+++ src/fr/ensimag/ihm/FenetreStatistiques.java
@@ -0,0 +1,108 @@
+package fr.ensimag.ihm;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+public class FenetreStatistiques extends JPanel implements ActionListener {
+    
+    private JLabel LabelNbStagiaires = new JLabel("Nombres de stagiaires : ");
+    private JLabel LabelSqlStagiaires = new JLabel("1");
+    private JLabel LabelRecettes = new JLabel("Total des recettes de l'association pour l'année en cours : ");
+    private JLabel LabelSqlRecettes = new JLabel("1 €");
+    private Box bv = Box.createVerticalBox();
+    
+    // boutons
+    private JButton boutonTerrains = new JButton("Terrains les plus utilisés");
+    private JButton boutonInscrits = new JButton("Nombre moyen d'inscrits par stage");
+    private JButton boutonRatio= new JButton("Ratio supervision/encadrement des moniteurs");
+    
+    public FenetreStatistiques() {
+        this.add(bv);
+        
+        // nombre de stagiaires
+        /*
+            Requête SQL pour attraper le nombre de stagiaires
+        */
+        Box boxNbStagiaires = Box.createHorizontalBox();
+        boxNbStagiaires.add(this.LabelNbStagiaires);
+        boxNbStagiaires.add(this.LabelSqlStagiaires);
+        this.bv.add(boxNbStagiaires);
+        
+                
+        // recettes de l'association
+        Box boxRecettes = Box.createHorizontalBox();
+        boxRecettes.add(this.LabelRecettes);
+        boxRecettes.add(this.LabelSqlRecettes);
+        this.bv.add(boxRecettes);
+
+        
+        this.boutonTerrains.addActionListener(this);
+        this.boutonInscrits.addActionListener(this);
+        this.boutonRatio.addActionListener(this);
+        
+        this.bv.add(boutonTerrains);
+        this.bv.add(boutonInscrits);
+        this.bv.add(boutonRatio);
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        if (arg0.getSource() == this.boutonTerrains) {
+            JFrame f = new JFrame("Terrains les plus utilisés");
+
+            String terrain = new String("Terrain");
+            
+            String[] data = new String[15];
+            for (int i = 0; i < 10 ; i++){
+                data[i] = terrain + i + " : nbUtilisation";
+            }
+            
+            f.add(new JList(data));
+            f.pack();
+            f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        }
+        if (arg0.getSource() == this.boutonInscrits) {
+            JFrame f = new JFrame("Nombre moyen d'inscrits par stage");
+
+            String stage = new String("Stage");
+            
+            String[] data = new String[15];
+            for (int i = 0; i < 10 ; i++){
+                data[i] = stage + i + " : nbInscrit";
+            }
+            
+            f.add(new JList(data));
+            f.pack();
+            f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        }
+        if (arg0.getSource() == this.boutonRatio) {
+            JFrame f = new JFrame("Ratio supervision/encadrement des moniteurs");
+            String moniteur = new String("Moniteur");
+            
+            String[] data = new String[15];
+            for (int i = 0; i < 10 ; i++){
+                data[i] = moniteur + i + " : ratio";
+            }
+          
+            f.add(new JList(data));
+            f.pack();
+            f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        }
+        
+       
+        this.setVisible(true);
+    }
+}
