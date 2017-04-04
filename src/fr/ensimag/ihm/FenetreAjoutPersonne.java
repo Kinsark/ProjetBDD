d9aa8726a738105b588d2ac6f34f0bcfc9d4aa16
Francis Shih <francis.shih@phelma.grenoble-inp.fr>
2017-04-04 15:17:59 +0200

    Added address form

--- src/fr/ensimag/ihm/FenetreAjoutPersonne.java
+++ src/fr/ensimag/ihm/FenetreAjoutPersonne.java
@@ -8,23 +8,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 
 public abstract class FenetreAjoutPersonne extends JPanel implements ActionListener {
 
-    private static final String REGEX_LETTRES = "[a-zA-Z]+";
+    private static final String REGEX_NOM = "[a-zA-Z]+";
     private static final String REGEX_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     private static final String REGEX_TELEPHONE = "[0-9]{10}";
-
+	private static final String REGEX_NUMERO_VOIE = "[1-9][0-9]*";
+	private static final String REGEX_NOM_VOIE = "[a-zA-Z ]+";
+	private static final String REGEX_COMMUNE = "[0-9]{5}";
+	
     private JOptionPane jop = new JOptionPane();
 	private int yPos = 0;
     
@@ -36,6 +42,12 @@ public abstract class FenetreAjoutPersonne extends JPanel implements ActionListe
     private JTextField tfEmail = new JTextField(20);
     private JLabel labelTelephone = new JLabel("Téléphone");
     private JTextField tfTelephone = new JTextField(10);
+	private JLabel labelNumeroVoie = new JLabel("Numéro dans la voie");
+    private JTextField tfNumeroVoie = new JTextField(10);
+	private JLabel labelNomVoie = new JLabel("Nom de la voie");
+    private JTextField tfNomVoie = new JTextField(10);
+	private JLabel labelCommune = new JLabel("Code postal");
+    private JTextField tfCommune = new JTextField(10);
     
     
     public FenetreAjoutPersonne() {
@@ -49,11 +61,13 @@ public abstract class FenetreAjoutPersonne extends JPanel implements ActionListe
 		panelGridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
 		panelGridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		this.setLayout(panelGridBagLayout);
-		
 		this.addLabelAndComponent(labelNom, tfNom);
 		this.addLabelAndComponent(labelPrenom, tfPrenom);
 		this.addLabelAndComponent(labelEmail, tfEmail);
 		this.addLabelAndComponent(labelTelephone, tfTelephone);
+		this.addLabelAndComponent(labelNumeroVoie, tfNumeroVoie);
+		this.addLabelAndComponent(labelNomVoie, tfNomVoie);
+		this.addLabelAndComponent(labelCommune, tfCommune);
     }
     
     public void actionPerformed(ActionEvent arg0){};
@@ -63,11 +77,11 @@ public abstract class FenetreAjoutPersonne extends JPanel implements ActionListe
     } 
     
     protected boolean verifierFormulaire() {
-        if (!this.tfNom.getText().matches(this.REGEX_LETTRES)) {
+        if (!this.tfNom.getText().matches(this.REGEX_NOM)) {
             jop.showMessageDialog(null, "Un nom est uniquement composé de lettres !", "Erreur", JOptionPane.ERROR_MESSAGE);
             return false;
         }
-        if (!this.tfPrenom.getText().matches(this.REGEX_LETTRES)) {
+        if (!this.tfPrenom.getText().matches(this.REGEX_NOM)) {
             jop.showMessageDialog(null, "Un prénom est uniquement composé de lettres !", "Erreur", JOptionPane.ERROR_MESSAGE);
             return false;
         }
@@ -79,24 +93,40 @@ public abstract class FenetreAjoutPersonne extends JPanel implements ActionListe
             jop.showMessageDialog(null, "Téléphone invalide (il faut 10 chiffres) !", "Erreur", JOptionPane.ERROR_MESSAGE);
             return false;
         }
+		if (!this.tfNumeroVoie.getText().matches(this.REGEX_NUMERO_VOIE)) {
+            jop.showMessageDialog(null, "Numéro de la voie invalide !", "Erreur", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+		if (!this.tfNomVoie.getText().matches(this.REGEX_NOM_VOIE)) {
+            jop.showMessageDialog(null, "Nom de la voie invalide !", "Erreur", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+		if (!this.tfCommune.getText().matches(this.REGEX_COMMUNE)) {
+            jop.showMessageDialog(null, "Code postal invalide !", "Erreur", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
         return true;
     }
 	
-	protected void addLabelAndComponent(JLabel label, Component comp) {
+	protected void addLabelAndComponent(JPanel pan, JLabel label, Component comp) {
 		GridBagConstraints gridBagConstraintForLabel = new GridBagConstraints();
 		gridBagConstraintForLabel.fill = GridBagConstraints.BOTH;
 		gridBagConstraintForLabel.insets = new Insets(0, 0, 5, 5);
 		gridBagConstraintForLabel.gridx = 0;
 		gridBagConstraintForLabel.gridy = this.yPos;
-		this.add(label, gridBagConstraintForLabel);
+		pan.add(label, gridBagConstraintForLabel);
 
 		GridBagConstraints gridBagConstraintForTextField = new GridBagConstraints();
 		gridBagConstraintForTextField.fill = GridBagConstraints.BOTH;
 		gridBagConstraintForTextField.insets = new Insets(0, 0, 5, 0);
 		gridBagConstraintForTextField.gridx = 1;
 		gridBagConstraintForTextField.gridy = this.yPos;
-		this.add(comp, gridBagConstraintForTextField);
+		pan.add(comp, gridBagConstraintForTextField);
 		
 		this.yPos++;
+	}
+	
+	protected void addLabelAndComponent(JLabel label, Component comp) {
+		this.addLabelAndComponent(this, label, comp);
 	}
 }
